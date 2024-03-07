1a8cb154c48b97d6e46d31c334a5d404cc64fc8e
iijawhor <jawhorali1@gmail.com>
2024-03-07 09:41:20 +0530

    added files to export

---
+++ src/exports/exports.js
@@ -0,0 +1,99 @@
+import HomePage from "../pages/HomePage/HomePage";
+import propertyData from "../data/propertyData";
+import Container from "../components/Container/Container";
+import LandingPage from "../pages/LandingPage/LandingPage";
+import Input from "../components/Input/Input";
+import Button from "../components/Button/Button";
+import SearchIcon from "@mui/icons-material/Search";
+import TrendingFlatIcon from "@mui/icons-material/TrendingFlat";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import CelebrationIcon from "@mui/icons-material/Celebration";
+import WalletIcon from "@mui/icons-material/Wallet";
+import LocationOnIcon from "@mui/icons-material/LocationOn";
+import FeaturedProperty from "../components/FeaturedProperty/FeaturedProperty";
+import featuredProperty from "../data/featuredProperty";
+import KingBedIcon from "@mui/icons-material/KingBed";
+import FamilyRestroomIcon from "@mui/icons-material/FamilyRestroom";
+import BathroomIcon from "@mui/icons-material/Bathroom";
+import CloseFullscreenIcon from "@mui/icons-material/CloseFullscreen";
+import LandingPageCard from "../components/LandingPageCard/LandingPageCard";
+import NewsAndConsult from "../components/NewsAndConsult/NewsAndConsult";
+import Footer from "../components/Footer/Footer";
+import Logo from "../components/Logo/Logo";
+import XIcon from "@mui/icons-material/X";
+import LinkedInIcon from "@mui/icons-material/LinkedIn";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import CopyrightIcon from "@mui/icons-material/Copyright";
+import KeyboardArrowDownIcon from "@mui/icons-material/KeyboardArrowDown";
+import Header from "../components/Header/Header";
+import Listing from "../pages/Listing/Listing";
+import PropertyDetail from "../pages/PropertyDetail/PropertyDetail";
+import AgentCard from "../components/AgentCard/AgentCard";
+import AgentList from "../pages/AgentList/AgentList";
+import AddIcon from "@mui/icons-material/Add";
+import AgentProfile from "../pages/AgentProfile/AgentProfile";
+import BlogCard from "../components/BlogCard/BlogCard";
+import BlogPage from "../pages/BlogPage/BlogPage";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import PasswordIcon from "@mui/icons-material/Password";
+import GoogleIcon from "@mui/icons-material/Google";
+import PrivacyPolicy from "../pages/PrivacyPolicy/PrivacyPolicy";
+import LicensePage from "../pages/LicensePage/LicensePage";
+import LoginPage from "../pages/Login/LoginPage";
+import CreateAccount from "../pages/CreateAccount/CreateAccount";
+import OTP from "../pages/OTP/OTP";
+import FaqPage from "../pages/FaqPage/FaqPage";
+import Error from "../components/Error/Error";
+import ArrowRightAltIcon from "@mui/icons-material/ArrowRightAlt";
+export {
+  Error,
+  FaqPage,
+  OTP,
+  CreateAccount,
+  LoginPage,
+  LicensePage,
+  PrivacyPolicy,
+  BlogCard,
+  BlogPage,
+  AgentProfile,
+  Listing,
+  AgentCard,
+  AgentList,
+  Header,
+  Logo,
+  HomePage,
+  propertyData,
+  Container,
+  LandingPage,
+  Input,
+  Button,
+  FeaturedProperty,
+  featuredProperty,
+  LandingPageCard,
+  NewsAndConsult,
+  Footer,
+  PropertyDetail
+};
+
+export {
+  ArrowRightAltIcon,
+  GoogleIcon,
+  PasswordIcon,
+  PersonOutlineIcon,
+  AddIcon,
+  TrendingFlatIcon,
+  LocationOnIcon,
+  SearchIcon,
+  VisibilityIcon,
+  WalletIcon,
+  CelebrationIcon,
+  KingBedIcon,
+  CloseFullscreenIcon,
+  BathroomIcon,
+  FamilyRestroomIcon,
+  InstagramIcon,
+  XIcon,
+  LinkedInIcon,
+  CopyrightIcon,
+  KeyboardArrowDownIcon
+};
