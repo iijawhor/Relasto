a44e140d65984ba5e3dc6875e633ce809eaf1a8a
iijawhor <jawhorali1@gmail.com>
2024-03-07 09:48:51 +0530

    implemented router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,41 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import {
+  Listing,
+  PropertyDetail,
+  AgentList,
+  AgentProfile,
+  BlogPage
+} from "./exports/exports";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+  {
+    path: "/listing",
+    element: <Listing />
+  },
+  {
+    path: "/property-detail/:id",
+    element: <PropertyDetail />
+  },
+  {
+    path: "/agent-list",
+    element: <AgentList />
+  },
+  {
+    path: "/blog-page",
+    element: <BlogPage />
+  }
+]);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
